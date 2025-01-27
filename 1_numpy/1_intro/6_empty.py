ed1de7f5ee3b416a038d26da5137d48bc0697bd6
Akanksha <akankshazc23@icloud.com>
2025-01-27 22:35:25 +0530

    add examples of creating empty 2D and 3D NumPy arrays

--- 1_numpy/1_intro/6_empty.py
+++ 1_numpy/1_intro/6_empty.py
@@ -1,6 +1,20 @@
 import numpy as np
 
 # create an empty array of length 4
-array1 = np.empty(4)
+array = np.empty(4)
 
+print(array)
+
+# n-d arrays with np.empty
+
+# create an empty 2D array with 2 rows and 2 columns
+array1 = np.empty((2, 2))
+
+print("2-D Array: ")
 print(array1)
+
+# create an empty 3D array of shape (2, 2, 2)
+array2 = np.empty((2, 2, 2))
+
+print("\n3-D Array: ")
+print(array2)
