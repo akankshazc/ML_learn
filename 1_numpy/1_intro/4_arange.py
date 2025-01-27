af50a2adb371e319a98b8cb88540694e37f94882
Akanksha <akankshazc23@icloud.com>
2025-01-27 22:35:13 +0530

    add examples of creating 2D and 3D NumPy arrays filled with zeros

--- 1_numpy/1_intro/4_arange.py
+++ 1_numpy/1_intro/4_arange.py
@@ -9,3 +9,17 @@ print("Using np.arange(5):", array1)
 array2 = np.arange(1, 9, 2)
 
 print("Using np.arange(1, 9, 2):", array2)
+
+# n-d arrays from np.zeros
+
+# create 2D array with 2 rows and 3 columns filled with zeros
+array3 = np.zeros((2, 3))
+
+print("2-D Array: ")
+print(array3)
+
+# create 3D array with dimensions 2x3x4 filled with zeros
+array4 = np.zeros((2, 3, 4))
+
+print("\n3-D Array: ")
+print(array4)
