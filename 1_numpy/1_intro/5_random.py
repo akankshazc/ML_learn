adaa0433059369f0f57c893beffce9a960028dd0
Akanksha <akankshazc23@icloud.com>
2025-01-27 22:35:19 +0530

    add examples of creating 2D and 3D arrays of random numbers using np.random.rand

--- 1_numpy/1_intro/5_random.py
+++ 1_numpy/1_intro/5_random.py
@@ -4,3 +4,17 @@ import numpy as np
 array = np.random.rand(5)
 
 print(array)
+
+# n-d arrays with np.random.rand
+
+# create a 2D array of 2 rows and 2 columns of random numbers
+array1 = np.random.rand(2, 2)
+
+print("2-D Array: ")
+print(array1)
+
+# create a 3D array of shape (2, 2, 2) of random numbers
+array2 = np.random.rand(2, 2, 2)
+
+print("\n3-D Array: ")
+print(array2)
