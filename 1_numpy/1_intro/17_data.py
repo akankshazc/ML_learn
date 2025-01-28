23220c25f603107f9ca734ab92ecc4fcd66e2ccc
Akanksha <akankshazc23@icloud.com>
2025-01-28 23:41:09 +0530

    add example of accessing the data attribute of NumPy arrays

---
+++ 1_numpy/1_intro/17_data.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+array1 = np.array([6, 7, 8])
+array2 = np.array([[1, 2, 3],
+                   [6, 7, 8]])
+
+# print memory address of array1's and array2's data
+print("\nData of array1 is: ", array1.data)
+print("Data of array2 is: ", array2.data)
