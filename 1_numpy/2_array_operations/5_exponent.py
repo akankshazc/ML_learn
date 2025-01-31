93037591da5af9e5a4f3c2d96bac92f855cf612f
Akanksha <akankshazc23@icloud.com>
2025-01-31 07:18:43 +0530

    add example of raising elements in a NumPy array to a power using ** operator and np.power() function

---
+++ 1_numpy/2_array_operations/5_exponent.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+array = np.array([1, 2, 3, 4, 5])
+
+# using the ** operator
+result1 = array ** 2
+print("Using the ** operator:", result1)
+
+# using the np.power() function
+result2 = np.power(array, 2)
+print("Using the np.power() function:", result2)
