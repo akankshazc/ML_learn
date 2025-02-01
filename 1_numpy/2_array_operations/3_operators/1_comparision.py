04d43334958870541b7791830de501133c1d3e34
Akanksha <akankshazc23@icloud.com>
2025-02-01 23:48:20 +0530

    add example of comparison operators for NumPy arrays

---
+++ 1_numpy/2_array_operations/3_operators/1_comparision.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+array1 = np.array([1, 2, 3])
+array2 = np.array([3, 2, 1])
+
+# less than operator
+result1 = array1 < array2
+print("array1 < array2:", result1)
+
+# greater than operator
+result2 = array1 > array2
+print("array1 > array2:", result2)
+
+# equal to operator
+result3 = array1 == array2
+print("array1 == array2:", result3)
