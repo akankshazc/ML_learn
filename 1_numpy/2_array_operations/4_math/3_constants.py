053f80c1719fbcfb00f74dccb90aeb772ffba757
Akanksha <akankshazc23@icloud.com>
2025-02-02 23:46:41 +0530

    add example of mathematical constants and operations using NumPy

---
+++ 1_numpy/2_array_operations/4_math/3_constants.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+# using the constant pi
+radius = 2
+circumference = 2 * np.pi * radius
+print(circumference)
+
+# using the constant e
+y = np.e
+print(y)
+
+# compute the exponential of a number
+x = 1
+y = np.exp(x)
+print(y)
+
+# add each array element with the constant pi
+array1 = np.array([1, 2, 3])
+y = array1 + np.pi
+print(y)
