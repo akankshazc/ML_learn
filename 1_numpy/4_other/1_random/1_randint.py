1138493ea58d8bc44660966bedce135fd4c1ff3b
Akanksha <akankshazc23@icloud.com>
2025-02-08 22:32:09 +0530

    add example of generating a random integer using numpy

---
+++ 1_numpy/4_other/1_random/1_randint.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+# to generate a random integer from 0 to 9
+# the lower bound is inclusive and the upper bound is exclusive
+random_number = np.random.randint(0, 10)
+
+print(random_number)
