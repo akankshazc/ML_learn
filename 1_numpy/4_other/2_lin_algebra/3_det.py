c582d35da582053dc8302f117d46ac2ae36d3eae
Akanksha <akankshazc23@icloud.com>
2025-02-09 23:41:56 +0530

    add example of computing the determinant of a square matrix

---
+++ 1_numpy/4_other/2_lin_algebra/3_det.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# define a square matrix
+array1 = np.array([[1, 3],
+                  [5, 7]])
+
+# compute the determinant of array1
+result = np.linalg.det(array1)
+
+
+print(result)
