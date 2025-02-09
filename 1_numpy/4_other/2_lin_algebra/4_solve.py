a8601874c9547f4cb0f28e508cb833535b36a214
Akanksha <akankshazc23@icloud.com>
2025-02-09 23:42:10 +0530

    add example of solving a system of linear equations

---
+++ 1_numpy/4_other/2_lin_algebra/4_solve.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# define the coefficient matrix A
+A = np.array([[2, 4],
+             [6, 8]])
+
+# define the constant vector b
+b = np.array([5, 6])
+
+# solve the system of linear equations Ax = b
+x = np.linalg.solve(A, b)
+
+print(x)
