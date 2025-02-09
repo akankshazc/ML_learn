92df00cf2ca066ef2a19d534929ceb0d8ed1c0a3
Akanksha <akankshazc23@icloud.com>
2025-02-09 23:42:28 +0530

    add example of computing the trace of a matrix

---
+++ 1_numpy/4_other/2_lin_algebra/6_trace.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# define a 3x3 matrix
+array1 = np.array([[6, 3, 5],
+                   [9, 2, 1],
+                   [7, 8, 4]])
+
+# compute the trace of the matrix
+result = np.trace(array1)
+
+print(result)
