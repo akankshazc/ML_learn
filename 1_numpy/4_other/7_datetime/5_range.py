de1adb460fe30e708ca8f04e2129bb6302963ade
Akanksha <akankshazc23@icloud.com>
2025-02-13 22:54:11 +0530

    add example of creating a range of dates using numpy's datetime64

---
+++ 1_numpy/4_other/7_datetime/5_range.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+# create a range of dates from 2023-04-01 to 2023-04-10
+dates = np.arange('2023-04-01', '2023-04-11', dtype='datetime64[D]')
+
+# print the dates
+print(dates)
