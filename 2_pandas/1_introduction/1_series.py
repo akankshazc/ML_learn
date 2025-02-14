be6361a9116b101ea3509534533a7f7a689f3e39
Akanksha <akankshazc23@icloud.com>
2025-02-14 05:42:44 +0530

    add example of creating a pandas Series

---
+++ 2_pandas/1_introduction/1_series.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# create a list
+data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# create a series
+s = pd.Series(data)
+
+# display the series
+print(s)
+
+# index labels in the series
+print(s[2])
