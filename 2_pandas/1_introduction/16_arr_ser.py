a5757b2f40a79cc263563545c327069a8af2924b
Akanksha <akankshazc23@icloud.com>
2025-02-14 05:45:10 +0530

    add example of creating a Pandas Series from a Pandas array

---
+++ 2_pandas/1_introduction/16_arr_ser.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# create a Pandas array
+arr = pd.array([1, 2, 3, 4, 5])
+
+# create a pandas series from the pandas array
+series = pd.Series(arr)
+
+# print the series
+print(series)
