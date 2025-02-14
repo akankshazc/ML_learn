bd6fb0f555c19e8926364012103c5d7c49cf2c56
Akanksha <akankshazc23@icloud.com>
2025-02-14 05:44:42 +0530

    add example of retrieving index and index values from a pandas DataFrame

---
+++ 2_pandas/1_introduction/13_get_index.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+# create a DataFrame
+data = {'Name': ['SecUnit', 'Mensah', 'Ratthi', 'Pin-lee', 'Gurathin', 'Bharadwaj', 'Volescu'],
+        'Age': [4, 40, 30, 35, 35, 45, 50],
+        'Occupation': ['Security', 'Team Leader', 'Biology Specialist', 'Lawyer', 'Systems Specialist', 'Scientist', 'Scientist'], }
+
+df = pd.DataFrame(data)
+
+# return index object
+print(df.index)
+
+# return index values
+print(df.index.values)
