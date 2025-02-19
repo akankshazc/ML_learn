27737951453f935243add57659977c5007f1c903
Akanksha <akankshazc23@icloud.com>
2025-02-19 06:20:14 +0530

    add example of writing a Pandas DataFrame to a CSV file

---
+++ 2_pandas/3_import_export/2_to_csv.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+# create a dictionary
+data = {'Patient': ['John', 'Jane', 'Anne', 'Bob'],
+        'Age': [25, 60, 35, 49],
+        'Diagnosis': ['Schizophrenia', 'Diabetes', 'Depression', 'Hypertension']}
+
+# create a DataFrame
+df = pd.DataFrame(data)
+
+# write dataframe to csv file
+df.to_csv('data4.csv', index=False)
