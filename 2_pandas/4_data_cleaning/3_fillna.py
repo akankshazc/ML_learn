368a4dccf7a7150dd00ab0830da05e5311447bcc
Akanksha <akankshazc23@icloud.com>
2025-02-20 23:02:47 +0530

    add example of using fillna() to fill missing values with column means in a DataFrame

---
+++ 2_pandas/4_data_cleaning/3_fillna.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+# define a dictionary with sample data which includes some missing values
+data = {
+    'A': [1, 2, 3, None, 5],
+    'B': [None, 2, 3, 4, 5],
+    'C': [1, 2, None, None, 5]
+}
+
+df = pd.DataFrame(data)
+
+print("Original Data:\n", df)
+
+# filling NaN values with the mean of each column
+df.fillna(df.mean(), inplace=True)
+
+print("\nData after filling NaN with mean:\n", df)
